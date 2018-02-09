686c083ba07817a97de0a66b387d43405310307d
Manuel Colmenero Navarro <manuelcolmenero@gmail.com>
2018-02-09 22:42:44 +0100

    Día 8 - Sesión 2 Cierre mapa google y uso de Picasso para caché de imagenes.

--- app/src/main/java/com/mcolmenero/madridshops/activity/MainActivity.kt
+++ app/src/main/java/com/mcolmenero/madridshops/activity/MainActivity.kt
@@ -1,14 +1,35 @@
 package com.mcolmenero.madridshops.activity
 
+import android.Manifest.permission.ACCESS_COARSE_LOCATION
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.mcolmenero.madridshops.R
+import com.mcolmenero.madridshops.domain.interactor.ErrorCompletion
+import com.mcolmenero.madridshops.domain.interactor.SuccessCompletion
+import com.mcolmenero.madridshops.domain.interactor.getallshops.GetAllShopsInteractor
+import com.mcolmenero.madridshops.domain.interactor.getallshops.GetAllShopsInteractorImpl
+import com.mcolmenero.madridshops.domain.model.Shops
+import com.mcolmenero.madridshops.fragment.ListFragment
+import com.mcolmenero.madridshops.router.Router
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var listFragment: ListFragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,9 +38,109 @@ class MainActivity : AppCompatActivity() {
 
         Log.d("App", "onCreate MainActivity")
 
-        // Puntero al fragment del mapa
-        //val fm: FragmentManager = supportFragmentManager.getFragment()
+        // Configuración del MapFragment
+        setupMap()
 
+        // Configuración del ListFragment
+        setupList()
+    }
+
+    private fun setupList() {
+        // Puntero al fragment del listado
+        listFragment = supportFragmentManager.findFragmentById(R.id.activity_main_list_fragment) as ListFragment
+
+    }
+
+    private fun setupMap() {
+
+        val getAllShopsInteractor: GetAllShopsInteractor = GetAllShopsInteractorImpl(this)
+
+        getAllShopsInteractor.execute(object : SuccessCompletion<Shops> {
+            override fun successCompletion(element: Shops) {
+                initializeMap(element)
+            }
+
+        }, object : ErrorCompletion{
+            override fun errorCompletion(errorMessage: String) {
+                Toast.makeText(baseContext, "Error", Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+
+    private var map: GoogleMap? = null
+
+    private fun initializeMap(shops: Shops) {
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.activity_main_map_fragment) as SupportMapFragment
+        mapFragment.getMapAsync({ mapa ->
+            Log.d("SUCCESS", "HABEMUS MAPA")
+
+            centerMapInPosition(mapa,40.416775, -3.703790)
+
+
+            mapa.uiSettings.isRotateGesturesEnabled = false
+            mapa.uiSettings.isZoomControlsEnabled = true
+
+            showUserPosition(baseContext, mapa)
+
+            map = mapa
+
+            addAllPins(shops)
+        })
+    }
+
+    fun centerMapInPosition(map: GoogleMap, latitude: Double, longitude: Double) {
+
+        val coordinate = LatLng(latitude, longitude)
+
+        // Genera la posicion de la camara
+        val cameraPosition = CameraPosition.Builder()
+                .target(coordinate)
+                .zoom(14f)
+                .build()
+
+        // Coloca la camara a raiz de la posicion
+        map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+    }
+
+    fun showUserPosition(context: Context, map: GoogleMap) {
+        if (ActivityCompat.checkSelfPermission(context, ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(context, ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+            // No han dado permisos para la localización
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION),
+                    10)
+
+            return
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == 10) {
+            try {
+                map?.isMyLocationEnabled = true
+            } catch (e: SecurityException) {
+
+            }
+        }
+    }
+
+    fun addAllPins(shops: Shops) {
+        for (index in 0 until shops.count()){
+            val shop = shops.get(index)
+            // TODO: addPin(map, shop.latitude, shop.longitude, shop.name)
+            addPin(map!!, 40.416775, -3.703790, shop.name)
+        }
+    }
+
+    fun addPin(map: GoogleMap, latitude: Double, longitude: Double, title: String) {
+        map.addMarker(MarkerOptions()
+                .position(LatLng(latitude, longitude))
+                .title(title))
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -32,9 +153,16 @@ class MainActivity : AppCompatActivity() {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+
+        Router().navigateFromMainActivityToPicassoActivity(this)
+
+        return true
+        /*
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
+
         }
+        */
     }
 }
